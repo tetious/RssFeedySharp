dc346c002a9c1a2777f2b43b7251d14300446845
Greg Lincoln <tetious@gmail.com>
2013-04-27 19:28:54 -0400

    cleanup js files, tweak bundles

--- RssFeedySharp/App_Start/BundleConfig.cs
+++ RssFeedySharp/App_Start/BundleConfig.cs
@@ -8,21 +8,14 @@ namespace RssFeedySharp
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                        "~/Scripts/vendor/modernizr-*",
+                        "~/Scripts/vendor/jquery-{version}.js",
+                        "~/Scripts/vendor/jquery.unobtrusive*",
+                        "~/Scripts/vendor/jquery.validate*",
+                        "~/Scripts/vendor/jquery.layout.js",
+                        "~/Scripts/vendor/bootstrap.js"));
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/site.css", 
                 "~/Content/bootstrap.css"));
